3de749b4fa65ef93470d0762e38ee9c9110cc8c1
Gourav Girish Shintre <gouravshintre02@gmail.com>
2024-10-29 17:14:08 +0530

    fixed app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter,Outlet,RouterProvider } from 'react-router-dom'
-import NavBar from './Navbar/NavBar'
+import NavBar from './navbar/NavBar'
 import './App.css'
 import './index.css'
 import Error_page from './error_page/Error_page'
@@ -7,7 +7,6 @@ import Home from './home/Home'
 import About from './about/About'
 import Contact from './contact/Contact'
 import Project from './projects/Project'
-import Skill from './skills/Skill'
 import FeedBack from './feedback/FeedBack'
 
 
